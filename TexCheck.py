c0f6130cbb64c45646cd73ef03a31a0edcdcbba9
Vespa <kylensherlock@gmail.com>
2015-01-03 01:35:25 +0800

    增加feacture: ref引用是否被定义

--- TexCheck.py
+++ TexCheck.py
@@ -13,37 +13,60 @@ def GetFileType(path,Filetype):
     		continue
     	for filename in files:
     		if len(re.findall(r".*%s$"%(Filetype),filename)) > 0 :
-    			yield path+r"/"+filename
+    			yield path+os.sep+filename
 
-def FindLabel(filename):
+def FindLabel(filename,Pre):
 	labelSet = [];
 	for (linenum,line) in enumerate(open(filename)):
-		result = re.findall("label{(.*)}",line)
+		result = re.findall("%s\{\s*([^\}]*)\s*\}"%(Pre),line)
 		if len(result):
 			labelSet.append([linenum+1,result[0]]);
 	return labelSet;
 
 
+def GetLabelDict(rootPath,Pre):
+	labelDict = {}
+	for filename in GetFileType(rootPath,"tex"):
+	    labelSet = FindLabel(filename,Pre)
+	    for line,label in labelSet:
+	    	if labelDict.has_key(label):
+	    		labelDict[label].append([line,filename])
+	    	else:
+	    		labelDict[label] = [[line,filename]]
+	return labelDict
 
-labelDict = {}
+############### Main
+if __name__ == "__main__":
+	rootPath = "./"
 
-rootPath = "./"
-for filename in GetFileType(rootPath,"tex"):
-    labelSet = FindLabel(filename)
-    for line,label in labelSet:
-    	if labelDict.has_key(label):
-    		labelDict[label].append([line,filename])
-    	else:
-    		labelDict[label] = [[line,filename]]
+	# Check Label repeat
+	labelDict = GetLabelDict(rootPath,"label")
+	hasLabelRepeat = False
+	for label in labelDict:
+		if len(labelDict[label]) > 1:
+			hasLabelRepeat = True
+			print "ERROR!!!    Label '%s' is Repeat in these files:"%label
+			for (line,filename) in labelDict[label]:
+				print "%s(Line %d)"%(filename,line)
+			print "\n"
 
-for label in labelDict:
-	if len(labelDict[label]) > 1:
-		print "Label '%s' is Repeat in these files:"%label
-		for (line,filename) in labelDict[label]:
-			print "%s(Line %d)"%(filename,line)
-		print "\n"
+	if not hasLabelRepeat:
+		print "[OK!]No Label Definition Repeat!!\n"
 
-print "Finished!!"
+	# Check Lable Defined
+	labelUsed = GetLabelDict(rootPath,"ref")
+	hasLabelUndefined = False
+	for label in labelUsed:
+		if not labelDict.has_key(label):
+			hasLabelUndefined = True
+			print "ERROR!!!    Label '%s' is used but not DEFINED!!:"%(label)
+			for (line,filename) in labelUsed[label]:
+				print "%s(Line %d)"%(filename,line)
+			print "\n"
 
+	if not hasLabelRepeat:
+		print "[OK!]All Label Used is Defined!!\n"
+
+	print "Finished!!"
 
 
