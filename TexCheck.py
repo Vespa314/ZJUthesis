47539c593fec70c91db4d46c02a5b1f2ab542bac
Vespa <kylensherlock@gmail.com>
2015-01-04 01:11:56 +0800

    Check weather label used in tex is defined in bib

--- TexCheck.py
+++ TexCheck.py
@@ -19,8 +19,8 @@ def FindLabel(filename,Pre):
 	labelSet = [];
 	for (linenum,line) in enumerate(open(filename)):
 		result = re.findall("%s\{\s*([^\}]*)\s*\}"%(Pre),line)
-		if len(result):
-			labelSet.append([linenum+1,result[0]]);
+		for item in result:
+			labelSet.append([linenum+1,item]);
 	return labelSet;
 
 
@@ -29,10 +29,11 @@ def GetLabelDict(rootPath,Pre):
 	for filename in GetFileType(rootPath,"tex"):
 	    labelSet = FindLabel(filename,Pre)
 	    for line,label in labelSet:
-	    	if labelDict.has_key(label):
-	    		labelDict[label].append([line,filename])
-	    	else:
-	    		labelDict[label] = [[line,filename]]
+	    	for labelsub in label.split(","):
+		    	if labelDict.has_key(labelsub):
+		    		labelDict[labelsub].append([line,filename])
+		    	else:
+		    		labelDict[labelsub] = [[line,filename]]
 	return labelDict
 
 def GetBibDict(rootPath):
@@ -96,21 +97,31 @@ if __name__ == "__main__":
 
 	# Check Bib Label Repeat
 	bibDict = GetBibDict(rootPath)
-	bibLabelUsed = GetLabelDict(rootPath,"cite")
-	for label in bibLabelUsed:
-		print label
-	hasBibLabelUndefined = False
+	hasBibLabelRepeat = False
 	for label in bibDict:
 		if len(bibDict[label]) > 1:
-			hasBibLabelUndefined = True
+			hasBibLabelRepeat = True
 			print "======================================================="
 			print "Bib Label %s is Redefined in these lines:\n"%(label)
 			for item in bibDict[label]:
-				print "<<%s>>\nLine:%s\nFile:%s\n"%(item[2],item[0],item[1])
+				print "<<%s>>\nLine:%d\nFile:%s\n"%(item[2],item[0],item[1])
 
+
+	if not hasBibLabelRepeat:
+		print "[OK!]No Label Definition Used in *bib is Repeat!!\n"
+
+	bibLabelUsed = GetLabelDict(rootPath,"cite")
+	hasBibLabelUndefined = False
+	for label in bibLabelUsed:
+		if not bibDict.has_key(label):
+			hasBibLabelUndefined = True
+			print "======================================================="
+			print "Bib Label %s is Used in These Lines BUT NOT BEEN DEFINED:"%(label)
+			for item in bibLabelUsed[label]:
+				print "File:%s(Line:%d)"%(item[1],item[0])
 
 	if not hasBibLabelUndefined:
-		print "[OK!]No Label Definition Used in *bib is Repeat!!\n"
+		print "[OK!]All Label Used in *tex is Defined in *bib!!\n"
 	print "Finished!!"
 
 
